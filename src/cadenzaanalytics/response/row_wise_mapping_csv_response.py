09a6ba85353168f8827afca9c9cfd4c128027623
dittmar <daniel.dittmar@disy.net>
2024-03-06 16:00:28 +0100

    allow returning plain ordered list of values in mapper instead of forcing to return pandas Series

--- src/cadenzaanalytics/response/row_wise_mapping_csv_response.py
+++ src/cadenzaanalytics/response/row_wise_mapping_csv_response.py
@@ -1,6 +1,6 @@
 from typing import Callable, List
 
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 from cadenzaanalytics.data.column_metadata import ColumnMetadata
 from cadenzaanalytics.response.error_response import ErrorResponse
@@ -20,7 +20,7 @@ class RowWiseMappingCsvResponse(ExtensionResponse):
                                  if c.attribute_group_name == KEY_ATTRIBUTE_GROUP_NAME]
         key_attribute_column_names = [c.name for c in key_attribute_columns]
         truncated_original_data = original_data.drop(key_attribute_column_names, axis=1)
-        result_data = truncated_original_data.apply(self._row_mapper, axis=1)
+        result_data = truncated_original_data.apply(self._apply_and_wrap, axis=1)
         if len(self._response_columns) != len(result_data.columns):
             return ErrorResponse(
                 f"Expected  {len(result_data.columns)} response columns but got {len(self._response_columns)}", 500)
@@ -30,3 +30,9 @@ class RowWiseMappingCsvResponse(ExtensionResponse):
         if self._closure is not None:
             self._closure(result_data)
         return response
+
+    def _apply_and_wrap(self, row):
+        mapped_row = self._row_mapper(row)
+        if isinstance(mapped_row, Series):
+            return mapped_row
+        return Series(mapped_row, index=[c.name for c in self._response_columns])
