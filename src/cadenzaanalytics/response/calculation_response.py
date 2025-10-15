d2ab9732dfca5d4a2f47b37cb070a2f9b70a7ebe
Julian Janßen <25066318+julianjanssen@users.noreply.github.com>
2025-10-15 18:01:14 +0200

    Add calculation response to cadenzaanyltics/response

---
+++ src/cadenzaanalytics/response/calculation_response.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from pandas import DataFrame
+
+from cadenzaanalytics.data.column_metadata import ColumnMetadata
+from cadenzaanalytics.response.csv_response import CsvResponse
+
+
+class CalculationResponse(CsvResponse):
+    """A class representing a calculation response from an extension.
+
+    Parameters
+    ----------
+    CsvResponse : type
+        The data response type from which CalculationResponse inherits.
+    """
+    def __init__(self, data: DataFrame, column_metadata: List[ColumnMetadata]):
+        super().__init__(data, column_metadata)
