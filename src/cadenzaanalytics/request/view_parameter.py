441cd39ffe6f8c9327c4a1ef86d831debf6d1f02
Julian Janßen <25066318+julianjanssen@users.noreply.github.com>
2025-11-18 20:08:36 +0100

    Reformat view parameter in ca/request

--- src/cadenzaanalytics/request/view_parameter.py
+++ src/cadenzaanalytics/request/view_parameter.py
@@ -1,5 +1,6 @@
 class ViewParameter:
-    """This class holds view parameters of the analytics request send by cadenza. This information include parameters such as width and height.
+    """This class holds view parameters of the analytics request send by cadenza.
+    This information include parameters such as width and height.
     """
     VIEW_WIDTH_PARAMETER_NAME = 'net.disy.cadenza.imageWidth'
     VIEW_HEIGHT_PARAMETER_NAME = 'net.disy.cadenza.imageHeight'
@@ -11,36 +12,40 @@ class ViewParameter:
         self._device_pixel_ratio = device_pixel_ratio
 
 
-    """Getter for view width.
-
-    Returns
-    -------
-    int
-        View width
-    """
     @property
     def width(self) -> int:
+        """Getter for view width.
+
+        Returns
+        -------
+        int
+            View width
+        """
+
         return self._width
 
 
-    """Getter for view height.
-
-    Returns
-    -------
-    int
-        View height
-    """
     @property
     def height(self) -> int:
+        """Getter for view height.
+
+        Returns
+        -------
+        int
+            View height
+        """
+
         return self._height
 
-    """Getter for view device pixel ratio.
 
-    Returns
-    -------
-    float
-        View device pixel ratio
-    """
     @property
     def device_pixel_ratio(self) -> float:
+        """Getter for view device pixel ratio.
+
+        Returns
+        -------
+        float
+            View device pixel ratio
+        """
+
         return self._device_pixel_ratio
