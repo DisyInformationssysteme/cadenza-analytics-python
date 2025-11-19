5be57167dc20d1039e1bc024c4d6bce487dbb424
dittmar <daniel.dittmar@disy.net>
2025-11-19 15:38:38 +0100

    feat: extend sample data extension to have an optional geometry attribute, more parameters and optionally add null rows to the input

--- examples/data/extension/example_extensions.py
+++ examples/data/extension/example_extensions.py
@@ -6,7 +6,17 @@ import cadenzaanalytics as ca
 
 
 def data_echo_analytics_function(request: ca.AnalyticsRequest):
-    return ca.DataResponse(request["table"].data, request["table"].metadata.groups['any_data'])
+    data = request["table"].data
+    metadata = request["table"].metadata
+    add_nulls = request.parameters["append_nulls"]
+    append_rows_count = request.parameters["append_rows_count"]
+    if append_rows_count < 1:
+        append_rows_count = 1
+    if add_nulls:
+        # add a column with None values if user requested it
+        added = pd.DataFrame([[None] * len(data.columns)] * append_rows_count, columns=data.columns)
+        data = pd.concat([data, added])
+    return ca.DataResponse(data, metadata.get_columns())
 
 
 any_attribute_group = ca.AttributeGroup(
@@ -17,13 +27,28 @@ any_attribute_group = ca.AttributeGroup(
     data_types=[ca.DataType.STRING, ca.DataType.INT64, ca.DataType.FLOAT64, ca.DataType.ZONEDDATETIME],
     min_attributes=1
 )
+any_geometry_group = ca.AttributeGroup(
+    name="any_geometry",
+    print_name="Any geometry",
+    data_types=[ca.DataType.GEOMETRY],
+    geometry_types=[ca.GeometryType.POINT, ca.GeometryType.MULTIPOINT,
+                    ca.GeometryType.LINESTRING, ca.GeometryType.MULTILINESTRING,
+                    ca.GeometryType.POLYGON, ca.GeometryType.MULTIPOLYGON],
+    max_attributes=1
+)
 
 data_echo_extension = ca.CadenzaAnalyticsExtension(
     relative_path="data-echo-extension",
     analytics_function=data_echo_analytics_function,
     print_name="Example Echo Data Extension",
     extension_type=ca.ExtensionType.DATA,
-    attribute_groups=[any_attribute_group]
+    attribute_groups=[any_attribute_group, any_geometry_group],
+    parameters=[ca.Parameter(name="append_nulls", print_name="Append row with null values", parameter_type=ca.ParameterType.BOOLEAN, default_value=False, required=False),
+                ca.Parameter(name="append_rows_count", print_name="How many rows with null values should be appended?", parameter_type=ca.ParameterType.INT64, default_value=1, required=True),
+                ca.Parameter(name="float_param", print_name="Not used float", parameter_type=ca.ParameterType.FLOAT64, required=False),
+                ca.Parameter(name="geom_param", print_name="Not used geometry", parameter_type=ca.ParameterType.GEOMETRY, required=False),
+                ca.Parameter(name="datetime_param", print_name="Not used datetime", parameter_type=ca.ParameterType.ZONEDDATETIME, required=False),
+                ca.Parameter(name="string_param", print_name="Not used string", parameter_type=ca.ParameterType.STRING, required=False)]
 )
 
 analytics_service = ca.CadenzaAnalyticsExtensionService()
