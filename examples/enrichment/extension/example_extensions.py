d3f4f4a9995575031426d03e88032284fa660661
dittmar <daniel.dittmar@disy.net>
2025-11-20 10:25:13 +0100

    feat: add convenience for enrichment response creation and add example that uses it

--- examples/enrichment/extension/example_extensions.py
+++ examples/enrichment/extension/example_extensions.py
@@ -25,6 +25,24 @@ def enrichment_basic_analytics_function(request: ca.AnalyticsRequest):
     return ca.EnrichmentResponse(df_data, result_metadata)
 
 
+def enrichment_signum_analytics_function(request: ca.AnalyticsRequest):
+    signum_column = ca.ColumnMetadata(name="signum", print_name="Signum",
+                                      data_type=ca.DataType.INT64,
+                                      role=ca.AttributeRole.DIMENSION)
+    def get_signum(row):
+        return pd.Series(
+            {signum_column.name: sign(row["number"])}, index=[signum_column.name]
+        )
+
+    def sign(value) -> int:
+        if value > 0:
+            return 1
+        if value < 0:
+            return -1
+        return 0
+
+    return request['table'].to_enrichment(new_columns=[signum_column], row_mapper=get_signum)
+
 any_attribute_group = ca.AttributeGroup(
     name="any_data",
     print_name="Any attribute",
@@ -32,6 +50,14 @@ any_attribute_group = ca.AttributeGroup(
     # non-geometry attribute is wanted from the user
     data_types=[ca.DataType.STRING, ca.DataType.INT64, ca.DataType.FLOAT64, ca.DataType.ZONEDDATETIME],
     min_attributes=1
+)
+
+any_number_group = ca.AttributeGroup(
+    name="number",
+    print_name="Any number",
+    data_types=[ca.DataType.INT64, ca.DataType.FLOAT64],
+    min_attributes=1,
+    max_attributes=1
 )
 
 enrichment_echo_extension = ca.CadenzaAnalyticsExtension(
@@ -42,8 +68,17 @@ enrichment_echo_extension = ca.CadenzaAnalyticsExtension(
     tables=[ca.Table(name="table", attribute_groups=[any_attribute_group])]
 )
 
+enrichment_signum_extension = ca.CadenzaAnalyticsExtension(
+    relative_path="signum-extension",
+    analytics_function=enrichment_signum_analytics_function,
+    print_name="Example Signum Enrichment Extension",
+    extension_type=ca.ExtensionType.ENRICHMENT,
+    tables=[ca.Table(name="table", attribute_groups=[any_number_group])]
+)
+
 analytics_service = ca.CadenzaAnalyticsExtensionService()
 analytics_service.add_analytics_extension(enrichment_echo_extension)
+analytics_service.add_analytics_extension(enrichment_signum_extension)
 
 if __name__ == '__main__':
     analytics_service.run_development_server(5005)
--- src/cadenzaanalytics/request/request_table.py
+++ src/cadenzaanalytics/request/request_table.py
@@ -1,6 +1,11 @@
+from typing import Callable, List
+
+import pandas as pd
 from pandas import DataFrame
 
 from cadenzaanalytics.request.request_metadata import RequestMetadata
+from cadenzaanalytics.response.missing_metadata_strategy import MissingMetadataStrategy
+from cadenzaanalytics.response.enrichment_response import ColumnMetadata, EnrichmentResponse
 
 
 class RequestTable:
@@ -31,3 +36,30 @@ class RequestTable:
             The data associated with the request.
         """
         return self._data
+
+    def to_enrichment(self, *,
+                      new_columns: List[ColumnMetadata],
+                      row_mapper: Callable[[pd.Series], pd.Series],
+                      missing_metadata_strategy: MissingMetadataStrategy=MissingMetadataStrategy.ADD_DEFAULT_METADATA) \
+            -> EnrichmentResponse:
+        """Creates an enrichment response by adding the given new columns and applying the mapper
+        on all data rows. This is a convenience method for creating enrichment responses where
+        each input row is mapped to a single output row.
+
+        :param new_columns: Description of the columns that are added as an enrichment to the cadenza objecttype.
+        :param row_mapper: Takes a row (pandas series) and returns a row (pandas series). The returned series
+        must have indices that correspond to the new_columns' names.
+        :param missing_metadata_strategy: Description of the strategy to handle missing metadata.
+        :return: An enrichment response.
+        """
+        result_data = self._data.apply(
+            row_mapper,
+            axis=1,
+            result_type='expand'
+        )
+        id_names = self._metadata.id_names
+        result_data[id_names] = self._data[id_names]
+
+        return EnrichmentResponse(result_data,
+                                  self._metadata.id_columns + new_columns,
+                                  missing_metadata_strategy=missing_metadata_strategy)
